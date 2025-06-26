c6ae770823f8c82f936944d2e119782275bfd178
Chien-Chin Huang <chienchin@fb.com>
2025-06-26 14:10:02 -0700

    Update
    [ghstack-poisoned]

--- kraken/all_reduce_fusion/__init__.py
+++ kraken/all_reduce_fusion/__init__.py
@@ -1,8 +1,17 @@
+from .rms_norm import rms_norm
 from .triton_one_shot_all_reduce_bias import (
     one_shot_all_reduce_bias as triton_one_shot_all_reduce_bias,
+)
+from .triton_one_shot_all_reduce_bias_rms_norm import (
+    one_shot_all_reduce_bias_rms_norm as triton_one_shot_all_reduce_bias_rms_norm,
 )
 from .triton_two_shot_all_reduce_bias import (
     two_shot_all_reduce_bias as triton_two_shot_all_reduce_bias,
 )
 
-__all__ = ["triton_one_shot_all_reduce_bias", "triton_two_shot_all_reduce_bias"]
+__all__ = [
+    "rms_norm",
+    "triton_one_shot_all_reduce_bias",
+    "triton_one_shot_all_reduce_bias_rms_norm",
+    "triton_two_shot_all_reduce_bias",
+]
