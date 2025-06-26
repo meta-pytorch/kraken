d0824b90508d8fb63b58fc6d8c26198ce8b9bfff
Chien-Chin Huang <chienchin@fb.com>
2025-06-25 23:41:27 -0700

    Update (base update)
    [ghstack-poisoned]

--- kraken/__init__.py
+++ kraken/__init__.py
@@ -1,4 +1,5 @@
 from . import all_reduce
+from . import all_reduce_fusion
 from . import _logging
 
-__all__ = ["all_reduce", "_logging"]
+__all__ = ["all_reduce", "all_reduce_fusion", "_logging"]
--- kraken/_logging/__init__.py
+++ kraken/_logging/__init__.py
@@ -1,3 +1,4 @@
 from .utils import log_triton_kernel as log_triton_kernel
+from .utils import plot_experiment_comparison
 from .benchmarking import benchmark_with_event as benchmark_with_event
 from .benchmarking import benchmark_with_profiler as benchmark_with_profiler
