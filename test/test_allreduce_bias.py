940aad837434033a13ceab4403e22e4e10104421
Chien-Chin Huang <chienchin@fb.com>
2025-07-02 15:33:25 -0700

    Update
    [ghstack-poisoned]

--- test/test_allreduce_bias.py
+++ test/test_allreduce_bias.py
@@ -12,7 +12,10 @@ from torch.testing._internal.common_utils import (
     run_tests,
 )
 
-from kraken.all_reduce_fusion import triton_one_shot_all_reduce_bias
+from kraken.all_reduce_fusion import (
+    triton_one_shot_all_reduce_bias,
+    triton_two_shot_all_reduce_bias,
+)
 
 
 @instantiate_parametrized_tests
@@ -68,6 +71,34 @@ class TritonAllReduceBiasTest(MultiProcessTestCase):
             bias = torch.randn(b, 5120, device=self.device, dtype=torch.bfloat16)
             y = torch.empty_like(input_tensor)
             triton_one_shot_all_reduce_bias(symm_mem_buffer, input_tensor, bias, y)
+            baseline = self._nccl_all_reduce_bias(input_tensor.clone(), bias.clone())
+
+            torch.testing.assert_close(y, baseline, rtol=5e-2, atol=5e-2)
+            dist.barrier()
+
+        dist.destroy_process_group()
+
+    @skip_if_lt_x_gpu(4)
+    def test_two_shot_bias(self):
+        self._init_process()
+
+        symm_mem_buffer = symm_mem.empty(
+            (1024, 1024),
+            dtype=torch.bfloat16,
+            device=self.device,
+        )
+        symm_mem.rendezvous(symm_mem_buffer, dist.group.WORLD)
+
+        for b in [1, 2, 4, 8, 16, 32, 64]:
+            torch.manual_seed(42 + self.rank + b)
+            input_tensor = torch.randn(
+                b, 5120, device=self.device, dtype=torch.bfloat16
+            )
+            # ensure the bias to be the same acorss rank
+            torch.manual_seed(42 + b)
+            bias = torch.randn(b, 5120, device=self.device, dtype=torch.bfloat16)
+            y = torch.empty_like(input_tensor)
+            triton_two_shot_all_reduce_bias(symm_mem_buffer, input_tensor, bias, y)
             baseline = self._nccl_all_reduce_bias(input_tensor.clone(), bias.clone())
 
             torch.testing.assert_close(y, baseline, rtol=5e-2, atol=5e-2)
