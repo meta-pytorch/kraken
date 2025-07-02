d6f361a747d3e6af197eed883684994fa6d43c0b
Chien-Chin Huang <chienchin@fb.com>
2025-07-02 14:24:02 -0700

    Implement one-shot allreduce bias fusion and benchmark
    ghstack-source-id: 19c9dfa8372f479c00d0e85c140d1c731b7e7d50
    Pull Request resolved: https://github.com/pytorch-labs/kraken/pull/6

--- kraken/__init__.py
+++ kraken/__init__.py
@@ -1,3 +1,3 @@
-from . import _logging, all_gather, all_reduce
+from . import _logging, all_gather, all_reduce, all_reduce_fusion
 
-__all__ = ["_logging", "all_gather", "all_reduce"]
+__all__ = ["_logging", "all_gather", "all_reduce", "all_reduce_fusion"]
---
+++ kraken/all_reduce_fusion/__init__.py
@@ -0,0 +1,3 @@
+from .triton_one_shot_all_reduce_bias import triton_one_shot_all_reduce_bias
+
+__all__ = ["triton_one_shot_all_reduce_bias"]
