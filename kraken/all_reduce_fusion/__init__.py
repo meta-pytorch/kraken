c4574c209df095e2f6b56a846b867bca8dc61af5
Chien-Chin Huang <chienchin@fb.com>
2025-07-03 16:38:28 -0700

    Implement one-shot allreduce, bias, RMSNorm fusion and benchmark
    ghstack-source-id: b1b47ad2ca9712ff3b003eaf42ff3f502ae1bf48
    Pull Request resolved: https://github.com/pytorch-labs/kraken/pull/8

--- kraken/all_reduce_fusion/__init__.py
+++ kraken/all_reduce_fusion/__init__.py
@@ -1,4 +1,13 @@
+from .rms_norm import rms_norm
 from .triton_one_shot_all_reduce_bias import triton_one_shot_all_reduce_bias
+from .triton_one_shot_all_reduce_bias_rms_norm import (
+    triton_one_shot_all_reduce_bias_rms_norm,
+)
 from .triton_two_shot_all_reduce_bias import triton_two_shot_all_reduce_bias
 
-__all__ = ["triton_one_shot_all_reduce_bias", "triton_two_shot_all_reduce_bias"]
+__all__ = [
+    "rms_norm",
+    "triton_one_shot_all_reduce_bias",
+    "triton_one_shot_all_reduce_bias_rms_norm",
+    "triton_two_shot_all_reduce_bias",
+]
