1cb1314a49d6bb88ea12c14635768d8debcdef92
Yunqiu Guo <guorachel@meta.com>
2025-07-28 00:04:08 -0700

    add benchmark for gemm + rs

--- kraken/__init__.py
+++ kraken/__init__.py
@@ -1,3 +1,3 @@
-from . import _logging, all_gather, all_reduce, all_reduce_fusion
+from . import _logging, all_gather, all_reduce, all_reduce_fusion, reduce_scatter_fusion
 
-__all__ = ["_logging", "all_gather", "all_reduce", "all_reduce_fusion"]
+__all__ = ["_logging", "all_gather", "all_reduce", "all_reduce_fusion", "reduce_scatter_fusion"]
