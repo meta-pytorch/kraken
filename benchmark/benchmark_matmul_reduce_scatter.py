8e3cf240cdf0a9cd87747bf5bad96e02928d665f
Surya Subramanian <73920354+codingwithsurya@users.noreply.github.com>
2025-10-09 13:59:59 -0700

    Update benchmark_matmul_reduce_scatter.py with right triton impl

--- benchmark/benchmark_matmul_reduce_scatter.py
+++ benchmark/benchmark_matmul_reduce_scatter.py
@@ -92,7 +92,7 @@ def get_single_backend_fn(backend: str):
     if backend == "torch_symm_mem":
         return torch_symm_mem_gemm_rs
     if backend == "triton":
-        return kraken.fused.gemm_reduce_scatter
+        return kraken.fused.gemm_reduce_scatter_ce_persistent
     raise NotImplementedError(backend)
 
 
